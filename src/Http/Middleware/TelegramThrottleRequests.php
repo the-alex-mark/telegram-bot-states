48e504a4e09ca1598bb6dde643dfe6217afba247
Макаров Александр Михайлович <the_alex_mark@mail.ru>
2022-05-25 08:35:29 +0300

    feature: Реализация посредника для ограничений по количеству отправляемых сообщений из одного чата (пользователя)

---
+++ src/Http/Middleware/TelegramThrottleRequests.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace ProgLib\Telegram\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Routing\Middleware\ThrottleRequests as BaseThrottleRequests;
+use Illuminate\Support\Carbon;
+use ProgLib\Telegram\Facades\TelegramCache;
+use RuntimeException;
+use Telegram\Bot\Answers\Answerable;
+use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Objects\Update;
+
+/**
+ * Обрабатывает запросы веб-перехватчика мессенджера «<b>Telegram</b>».
+ */
+class TelegramThrottleRequests extends BaseThrottleRequests {
+
+    use Answerable;
+
+    #region Helpers
+
+    /**
+     * Возвращает форматированное время.
+     *
+     * @param  int $seconds Количество секунд.
+     * @return string
+     */
+    protected function formattedRetryAfter($seconds) {
+        $time    = (new Carbon("00:00:00"))->modify("+ $seconds seconds");
+        $minutes = (int)$time->format('i');
+        $seconds = (int)$time->format('s');
+
+        return (($minutes > 0) ? $this->formattedMinutes($minutes) : '') . (($seconds > 0) ? (($minutes > 0) ? ' и ' : '') . $this->formattedSeconds($seconds) : '');
+    }
+
+    /**
+     * Возвращает форматированные время в секундах.
+     *
+     * @param  int $minutes Количество минут.
+     * @return string
+     */
+    protected function formattedMinutes($minutes) {
+        return $minutes . ' ' . trans_choice('минуту|минуты|минут', $minutes);
+    }
+
+    /**
+     * Возвращает форматированные время в минутах.
+     *
+     * @param  int $seconds Количество секунд.
+     * @return string
+     */
+    protected function formattedSeconds($seconds) {
+        return $seconds . ' ' . trans_choice('секунду|секунды|секунд', $seconds);
+    }
+
+    #endregion
+
+    /**
+     * @inheritDoc
+     */
+    protected function resolveRequestSignature($request) {
+        $chat = $this->update->getChat();
+
+        if ($chat->has('id'))
+            return sha1($chat->get('id'));
+
+        throw new RuntimeException('Невозможно сформировать подпись запроса. Маршрут недоступен.');
+    }
+
+    /**
+     * Обрабатывает запросы веб-перехватчика мессенджера «<b>Telegram</b>».
+     * Выполняет ограничение по количеству принимаемых запросов за указанное время.
+     *
+     * @inheritDoc
+     */
+    public function handle($request, Closure $next, $maxAttempts = 50, $decayMinutes = 1, $prefix = 'telegram') {
+        $this->telegram = Telegram::bot();
+        $this->update   = Update::make($request->all());
+
+        try { $key = $prefix . $this->resolveRequestSignature($request); }
+        catch (Exception $e) {
+            return response()->json([ 'ok' => false, 'description' => $e->getMessage() ]);
+        }
+
+        $responseCallback = function ($request, $headers) use ($key, $decayMinutes) {
+            TelegramCache::remember($key, 10, function () use ($key, $decayMinutes) {
+                $retryAfter = $this->getTimeUntilNextRetry($key);
+                $message = trans('telegram.messages.too_many_attempts', [
+                    'decay' => $this->formattedRetryAfter($decayMinutes * 60),
+                    'retry' => $this->formattedRetryAfter($retryAfter)
+                ]);
+
+                return $this->replyWithMessage([
+                    'text' => $message
+                ]);
+            });
+
+            return response()->json([ 'ok' => false, 'description' => 'Too Many Attempts' ]);
+        };
+
+        return $this->handleRequest($request, $next, [
+            (object)[
+                'key'              => $key,
+                'maxAttempts'      => $this->resolveMaxAttempts($request, $maxAttempts),
+                'decayMinutes'     => $decayMinutes,
+                'responseCallback' => $responseCallback,
+            ],
+        ]);
+    }
+}
