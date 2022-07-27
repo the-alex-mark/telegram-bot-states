4711db198e856a40044f0ff16bfcd527739e72a6
Макаров Александр Михайлович <the_alex_mark@mail.ru>
2022-07-28 00:46:43 +0300

    style: Рефакторинг конфигурации сервиса

--- config/telegram.php
+++ config/telegram.php
@@ -32,8 +32,8 @@ return [
 
     # Предопределённые параметры сообщения
     'message' => [
-        'parse_mode' => env('TELEGRAM_PARSE_MODE', 'MarkdownV2'),
-        'disable_web_page_preview' => true,
+        'parse_mode' => env('TELEGRAM_BOT_PARSE_MODE', 'MarkdownV2'),
+        'disable_web_page_preview' => env('TELEGRAM_BOT_DISABLE_PREVIEW', true)
     ],
 
     /*
@@ -46,13 +46,24 @@ return [
     */
 
     'options' => [
-        'debug' => env('TELEGRAM_DEBUG', false)
+
+        # Отладка запросов к API
+        'debug' => env('TELEGRAM_BOT_DEBUG', false),
+
+        # Ограничение по количеству запросов на веб-перехватчик
+        'throttle' => [
+            'enabled' => env('TELEGRAM_BOT_THROTTLE', false),
+            'attempts' => env('TELEGRAM_BOT_THROTTLE_ATTEMPTS'),
+            'during' => env('TELEGRAM_BOT_THROTTLE_DURING')
+        ]
     ],
 
+    # Ссылки на ресурсы мессенджера
     'endpoints' => [
         'site' => env('TELEGRAM_URL_SITE', 'https://telegram.org'),
         'messenger' => env('TELEGRAM_URL_MESSENGER', 'https://t.me'),
-        'api' => env('TELEGRAM_URL_API', 'https://api.telegram.org')
+        'api' => env('TELEGRAM_URL_API', 'https://api.telegram.org'),
+        'core' => env('TELEGRAM_URL_CORE', 'https://my.telegram.org')
     ],
 
     /*
